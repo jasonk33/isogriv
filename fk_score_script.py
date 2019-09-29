bd7e86e7bfb58d513e4a04a5edd5d09bcae27042
jasonk33 <jasonk33@gmail.com>
2019-09-29 19:16:10 -0400

    refactoring

--- fk_score_script.py
+++ fk_score_script.py
@@ -2,6 +2,7 @@ import pandas as pd
 from readability import Readability
 import sys
 import csv
+import json
 csv.field_size_limit(sys.maxsize)
 
 description_features = {}
@@ -12,7 +13,7 @@ with open("detail_description_data/detail-desc-text-{}.tsv".format(sys.argv[1]),
         detail_description_text = row[1]
         try:
             description_word_count = len(detail_description_text.split())
-            fig_counts = detail_description_text.count("fig.") + detail_description_text.count("figs.")
+            fig_counts = detail_description_text.lower().count("fig.") + detail_description_text.lower().count("figs.")
             r = Readability(detail_description_text.replace('aed-512', ''))
             fk_score = r.flesch_kincaid().score
         except:
@@ -20,4 +21,4 @@ with open("detail_description_data/detail-desc-text-{}.tsv".format(sys.argv[1]),
         description_features[patent_id] = (description_word_count, fk_score, fig_counts)
 with open("probability_data/description_features_{}.json".format(sys.argv[1]), 'w') as f:
     json.dump(description_features, f)
-	
+    
